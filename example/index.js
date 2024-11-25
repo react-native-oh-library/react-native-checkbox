694ff4dcd9c31ab83c142c319c38258609bc167e
longzi <zhengjinshouLL@163.com>
2024-11-25 10:31:56 +0800

    build: 修改版权头 (#20)
    Co-authored-by: zhengjinshou <zhengjinshou@huawei.com>

--- example/index.js
+++ example/index.js
@@ -1,5 +1,5 @@
 import {AppRegistry, View, Text} from 'react-native';
 import {name as appName} from './app.json';
-import App from './src/checkBoxTest';
+import App from './src/checkboxTest';
 
 AppRegistry.registerComponent(appName, () => App);
