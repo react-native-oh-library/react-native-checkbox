070b4a7df302f5f70a55a96ee29ad3f4b01e9e4f
zhengjinshou <zhengjinshou@huawei.com>
2024-11-25 10:20:25 +0800

    build: 修改版权头

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,7 @@
+// Copyright (c) 2019 react-native-community. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+// 2024.11.23 react-native 新架构适配
+//     Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved.
+
   export * as CheckBox from '@react-native-community/checkbox/typings/index'
